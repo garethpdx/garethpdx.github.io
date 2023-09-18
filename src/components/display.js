9149e44078bc4436f1dae853438550af87071fff
gareth <gareth@gareths-MacBook-Air.local>
2023-09-18 16:05:51 -0600

    default message with instructions

--- src/components/display.js
+++ src/components/display.js
@@ -1,6 +1,9 @@
 import React from 'react';
 
 const Display = ({ display }) => {
+    if(display == ''){
+	display = 'click something';
+    }
     return (
 	<div className="bg-gray-200 p-4">
 	{display}
