0975669a56d728c1f481418981d64b1386615649
gareth <gareth@gareths-MacBook-Air.local>
2023-09-19 11:46:54 -0600

    Additional config for static hosting

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    output: "export",
+    basePath: "/garethpdx.github.io",
+    images: {
+      unoptimized: true,
+    },
+
+}
 
 module.exports = nextConfig
