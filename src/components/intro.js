3f5bcdee5e4425e584f7939f2fdb9b5b9f89412f
gareth <gareth@gareths-MacBook-Air.local>
2023-09-19 11:28:22 -0600

    add a header

---
+++ src/components/intro.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Intro = () => {
+      return (
+          <div className="text-center py-5">
+	      <h2 className="text-3xl">Gareth Mackrill&apos;s Interactive Resume</h2>
+	      <p><a href="https://www.linkedin.com/in/gareth-m-software-engineering/">Get in touch</a></p>
+	  </div>
+      );
+};
+
+export default Intro;
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,17 +3,20 @@ import React, { useState } from 'react';
 
 import Display from '../components/display';
 import Form from '../components/form';
+import Intro from '../components/intro';
+
 
 const IndexPage = () => {
     const [content, setContent] = useState('');
     const handleToggle = (description) => {
 	setContent((prevContent) => description);
     };
-    return (
-	<div>
-	    <Display display={content} />
-	    <Form onToggle={handleToggle} />
-	</div>
+    return (	
+	    <div>
+		<Intro />
+		<Display display={content} />
+		<Form onToggle={handleToggle} />
+	    </div>
     );
 };
 export default IndexPage;
