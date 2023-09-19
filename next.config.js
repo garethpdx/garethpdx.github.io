5af931a69ad779b95f6ffd80cfb77a3b50de31cc
gareth <gareth@gareths-MacBook-Air.local>
2023-09-19 12:47:59 -0600

    github's jekyll isn't building files in _next

--- next.config.js
+++ next.config.js
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",
-    basePath: "/garethpdx.github.io",
     images: {
       unoptimized: true,
     },
-
 }
 
 module.exports = nextConfig
