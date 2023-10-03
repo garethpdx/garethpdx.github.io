fb7c902e899306b96673f3ce09a35a83566777bc
gareth <gareth@gareths-MacBook-Air.local>
2023-10-02 20:48:39 -0600

    Add google analytics

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import '../app/globals.css'
 
 import React, { useState } from 'react';
 
+import GA from '../components/ga4.js';
 import Display from '../components/display';
 import Form from '../components/form';
 import Intro from '../components/intro';
@@ -14,6 +15,7 @@ const IndexPage = () => {
     };
     return (	
 	    <div>
+		<GA />
 		<Intro />
 		<Display display={content} />
 		<Form onToggle={handleToggle} />
